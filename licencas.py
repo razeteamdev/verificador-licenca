06f88eef6e7cbb2a466bbc7267295a79c4cf3061
razeteamdev <razeteamdev@gmail.com>
2025-06-11 09:23:27 -0300

    Create licencas.py

---
+++ licencas.py
@@ -0,0 +1,12 @@
+{
+  "licencas_validas": [
+    {
+      "RAZE-GEKC-3IVY-R59E",
+      "mac": "d0:6f:4a:ef:c8:c6"
+    },
+    {
+      "chave": "RAZE-WXYZ-5678",
+      "mac": "11:22:33:44:55:66"
+    }
+  ]
+}
